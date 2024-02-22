2a0c0bb16ce45d7f8751842cae7ed69a99327412
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-02-22 19:36:06 +0200

    Revert "Update Contacts.jsx"
    This reverts commit c8a9ce24acbaa85bece697fb8ea432717138b66d.

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -39,20 +39,16 @@ const Contacts = () => {
             </div>
           </div>
           <div className="col-md-6 wow fadeIn" data-wow-delay="0.1s">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d715.1615352787768!2d23.377164180096038!3d42.63940002112274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa86c084796deb%3A0x11ee401206e440ee!2zZy5rLiBNbGFkb3N0IDNNbGFkb3N0LCDQti7Qui4g0JzQu9Cw0LTQvtGB0YIgMyAzMjksIDE3MTIgU29maWE!5e0!3m2!1sen!2sbg!4v1708505094888!5m2!1sen!2sbg"
-              style={{ minHeight: 350, border: 0 }}
-              className="position-relative rounded w-100 h-100"
-              allowFullScreen=""
-              aria-hidden="false"
-              tabIndex={0}
-              allowfullscreen=""
-              loading="lazy"
-            ></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d715.1615352787768!2d23.377164180096038!3d42.63940002112274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa86c084796deb%3A0x11ee401206e440ee!2zZy5rLiBNbGFkb3N0IDNNbGFkb3N0LCDQti7Qui4g0JzQu9Cw0LTQvtGB0YIgMyAzMjksIDE3MTIgU29maWE!5e0!3m2!1sen!2sbg!4v1708505094888!5m2!1sen!2sbg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
           <div className="col-md-6">
             <div className="wow fadeInUp" data-wow-delay="0.2s">
-              <h3 style={{textAlign: "center", marginBottom: "1em"}}>Contact our support team now!</h3>
+              <p className="mb-4">
+                The contact form is currently inactive. Get a functional and working
+                contact form with Ajax &amp; PHP in a few minutes. Just copy and
+                paste the files, add a little code and you're done.{" "}
+                <a href="https://htmlcodex.com/contact-form">Download Now</a>.
+              </p>
               <form>
                 <div className="row g-3">
                   <div className="col-md-6">
@@ -101,10 +97,7 @@ const Contacts = () => {
                     </div>
                   </div>
                   <div className="col-12">
-                    <button
-                      className="btn btn-primary w-100 py-3"
-                      type="submit"
-                    >
+                    <button className="btn btn-primary w-100 py-3" type="submit">
                       Send Message
                     </button>
                   </div>
