0cd1d392ba7f1e7aaadb95b046cc10f60a42ce38
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-04 00:16:24 +0200

    Create usePersistedState.jsx

---
+++ src/hooks/usePersistedState.jsx
@@ -0,0 +1,26 @@
+export default function usePersistedState(key, defaultValue) {
+    const [state, setState] = useState(()=>{
+        const persistedState = localStorage.getItem(key);
+
+        if(persistedState){
+            return JSON.parse(persistedState);
+        }
+
+        return defaultValue;
+    });
+
+    const setPersistedState = (value) => {
+        let result;
+
+        setState(value);
+
+        if(typeof value === 'function'){
+            result = JSON.stringify(value(state));
+        }else{
+            result = JSON.stringify(value);
+        }
+
+        localStorage.setItem(key, result);
+    };
+    return [state, setPersistedState];
+};
