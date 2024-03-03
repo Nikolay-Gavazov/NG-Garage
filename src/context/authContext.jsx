cc17489b48a3ba49e000d39034504a8e7b6064ed
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-04 00:16:18 +0200

    Create authContext.jsx

---
+++ src/context/authContext.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+import usePersistedState from "../hooks/usePersistedState";
+import { login } from "../services/userService";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({children}) =>{
+    const [isLoading, setIsloading] = useState(false);
+    const [auth, setAuth] = usePersistedState("auth", {});
+    const [serverError, setServerError] = useState("");
+
+    const navigate = useNavigate();
+
+    const loginSubmitHandler = async (data) => {
+        setIsloading(true);
+
+        try {
+            const result = await login(data.email, data.password);
+            setAuth(result);
+            localStorage.setItem("accessToken", result.token);
+            navigate("/");
+            setIsloading(false);
+            setServerError("");
+        } catch (error) {
+            setServerError(error.message);
+            setIsloading(false);
+        }
+    };
+
+    const registerSubmitHandler = async (data) => {
+        setIsloading(true);
+
+        try {
+            const result = await register(data.email, data.password);
+            setAuth(result);
+            localStorage.setItem("accessToken", result.token);
+            navigate("/");
+            setIsloading(false);
+            setServerError("");
+        } catch (error) {
+            setServerError(error.message);
+            setIsloading(false);
+        }
+    };
+
+    const logoutHandler = () => {
+        setIsloading(true);
+        setAuth({});
+        localStorage.removeItem("accessToken");
+        navigate("/");
+        setIsloading(false);
+    };
+
+    const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        email: auth.email,
+        userId: auth._id,
+        isAuthenticated: !!auth.token,
+        serverError,
+        isLoading
+    }
+
+    return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
