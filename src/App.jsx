1944aae458e41079f036a004e5f9f3a6bedc446d
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-05 00:31:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './styles/style.css'
 import Home from './components/pages/home/Home'
 import Topbar from './components/pages/layout/Topbar'
 import Navbar from './components/pages/layout/Navbar'
 import Footer from './components/pages/layout/Footer'
-import { Route, Routes } from 'react-router-dom'
 import Login from './components/pages/user/Login'
 import Register from './components/pages/user/Register'
 import About from './components/pages/About'
@@ -13,11 +12,16 @@ import Contacts from './components/pages/Contacts'
 import NotFound from './components/pages/NotFound'
 import Logout from './components/pages/user/Logout'
 import Service from './components/pages/home/Service'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import { AuthProvider } from './context/authContext'
+import AuthGuard from './guards/AuthGuard'
 
 function App() {
 
     return (
-        <>
+        <ErrorBoundary>
+            <AuthProvider>
+
             <Topbar />
             <Navbar />
 
@@ -25,16 +29,20 @@ function App() {
                 <Route path='/' element={<Home/>}/>
                 <Route path='/login' element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
-                <Route path='/logout' element={<Logout/>}/>
                 <Route path='/about' element={<About/>}/>
-                <Route path='/booking' element={<Booking/>}/>
                 <Route path='/contact' element={<Contacts/>}/>
                 <Route path='/services' element={<Service/>}/>
+
+                <Route element={<AuthGuard/>}>
+                    <Route path='/logout' element={<Logout/>}/>
+                    <Route path='/booking' element={<Booking/>}/>
+                </Route>
                 <Route path='*' element={<NotFound/>}/>
             </Routes>
 
             <Footer />
-        </>
+            </AuthProvider>
+        </ErrorBoundary>
     )
 }
 
