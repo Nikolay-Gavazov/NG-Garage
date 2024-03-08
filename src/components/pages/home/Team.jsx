038ebd2cf29fd2eecb535f788111ce29c124f9c8
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-09 00:02:23 +0200

    Update Team.jsx

--- src/components/pages/home/Team.jsx
+++ src/components/pages/home/Team.jsx
@@ -1,31 +1,66 @@
 const Team = () =>{
+  
     return(
+      <>
+        <div className="container-xxl py-5">
+          <div className="container">
+          <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+      <h6 className="text-primary text-uppercase">// Our Manager //</h6>
+      <h1 className="mb-5">Our Manager and Consultant</h1>
+    </div>
+    <div className="row g-4 flex justify-content-center margin-bottom-s">
+    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
+        <div className="team-item">
+          <div className="position-relative overflow-hidden">
+            <img className="img-fluid" src="img/team-4.jpg" alt="" />
+            <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
+              <a className="btn btn-square mx-1" target="blank" href="https://www.facebook.com/">
+                <i className="fab fa-facebook-f" />
+              </a>
+              <a className="btn btn-square mx-1" target="blank" href="https://twitter.com/">
+                <i className="fab fa-twitter" />
+              </a>
+              <a className="btn btn-square mx-1" target="blank" href="https://www.instagram.com/">
+                <i className="fab fa-instagram" />
+              </a>
+            </div>
+          </div>
+          <div className="bg-light text-center p-4">
+            <h5 className="fw-bold mb-0">Nikolay Gavazov</h5>
+            <small>General Consultant and Manager</small>
+          </div>
+        </div>
+      </div>
+    </div>
+          </div>
+
+        </div>
         <div className="container-xxl py-5">
   <div className="container">
     <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
       <h6 className="text-primary text-uppercase">// Our Technicians //</h6>
       <h1 className="mb-5">Our Expert Technicians</h1>
     </div>
-    <div className="row g-4">
+    <div className="row g-4 flex justify-content-center">
       <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
         <div className="team-item">
           <div className="position-relative overflow-hidden">
             <img className="img-fluid" src="img/team-1.jpg" alt="" />
             <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://www.facebook.com/">
                 <i className="fab fa-facebook-f" />
               </a>
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://twitter.com/">
                 <i className="fab fa-twitter" />
               </a>
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://www.instagram.com/">
                 <i className="fab fa-instagram" />
               </a>
             </div>
           </div>
           <div className="bg-light text-center p-4">
-            <h5 className="fw-bold mb-0">Full Name</h5>
-            <small>Designation</small>
+            <h5 className="fw-bold mb-0">Ivan Petrov</h5>
+            <small>Diagnostic Specialist</small>
           </div>
         </div>
       </div>
@@ -34,20 +69,20 @@ const Team = () =>{
           <div className="position-relative overflow-hidden">
             <img className="img-fluid" src="img/team-2.jpg" alt="" />
             <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://www.facebook.com/">
                 <i className="fab fa-facebook-f" />
               </a>
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://twitter.com/">
                 <i className="fab fa-twitter" />
               </a>
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://www.instagram.com/">
                 <i className="fab fa-instagram" />
               </a>
             </div>
           </div>
           <div className="bg-light text-center p-4">
-            <h5 className="fw-bold mb-0">Full Name</h5>
-            <small>Designation</small>
+            <h5 className="fw-bold mb-0">Petur Dimitrov</h5>
+            <small>Engine Servicing Specialist</small>
           </div>
         </div>
       </div>
@@ -56,48 +91,27 @@ const Team = () =>{
           <div className="position-relative overflow-hidden">
             <img className="img-fluid" src="img/team-3.jpg" alt="" />
             <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://www.facebook.com/">
                 <i className="fab fa-facebook-f" />
               </a>
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://twitter.com/">
                 <i className="fab fa-twitter" />
               </a>
-              <a className="btn btn-square mx-1" href="">
+              <a className="btn btn-square mx-1" target="blank" href="https://www.instagram.com/">
                 <i className="fab fa-instagram" />
               </a>
             </div>
           </div>
           <div className="bg-light text-center p-4">
-            <h5 className="fw-bold mb-0">Full Name</h5>
-            <small>Designation</small>
-          </div>
-        </div>
-      </div>
-      <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-        <div className="team-item">
-          <div className="position-relative overflow-hidden">
-            <img className="img-fluid" src="img/team-4.jpg" alt="" />
-            <div className="team-overlay position-absolute start-0 top-0 w-100 h-100">
-              <a className="btn btn-square mx-1" href="">
-                <i className="fab fa-facebook-f" />
-              </a>
-              <a className="btn btn-square mx-1" href="">
-                <i className="fab fa-twitter" />
-              </a>
-              <a className="btn btn-square mx-1" href="">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-          </div>
-          <div className="bg-light text-center p-4">
-            <h5 className="fw-bold mb-0">Full Name</h5>
-            <small>Designation</small>
+            <h5 className="fw-bold mb-0">Georgi Borisov</h5>
+            <small>Tire Specialist</small>
           </div>
         </div>
       </div>
     </div>
   </div>
 </div>
+      </>
     );
 };
 export default Team;
