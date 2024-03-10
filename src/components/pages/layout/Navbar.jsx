985adaa8b14c9f0f390d649faec322e77391ca7c
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-10 22:41:17 +0200

    Update Navbar.jsx

--- src/components/pages/layout/Navbar.jsx
+++ src/components/pages/layout/Navbar.jsx
@@ -1,10 +1,21 @@
-import { Link, NavLink } from "react-router-dom"
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import AuthContext from "../../../context/authContext";
 
 const Navbar = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-      <NavLink to="/" className="navbar-brand d-flex align-items-center px-4 px-lg-5">
-        <h2 className="m-0 text-primary"><i className="fa fa-car me-3" />NG Garage</h2></NavLink>
+      <NavLink
+        to="/"
+        className="navbar-brand d-flex align-items-center px-4 px-lg-5"
+      >
+        <h2 className="m-0 text-primary">
+          <i className="fa fa-car me-3" />
+          NG Garage
+        </h2>
+      </NavLink>
       <button
         type="button"
         className="navbar-toggler me-4"
@@ -15,31 +26,64 @@ const Navbar = () => {
       </button>
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <div className="navbar-nav ms-auto p-4 p-lg-0">
-          <NavLink to="/" className="nav-item nav-link" activeclassname = "active">
+          <NavLink
+            to="/"
+            className="nav-item nav-link"
+            activeclassname="active"
+          >
             Home
           </NavLink>
-          <NavLink to="/about" className="nav-item nav-link" activeclassname = "active">
+          <NavLink
+            to="/about"
+            className="nav-item nav-link"
+            activeclassname="active"
+          >
             About
           </NavLink>
-          <NavLink to="/contact" className="nav-item nav-link" activeclassname = "active">
+          <NavLink
+            to="/contact"
+            className="nav-item nav-link"
+            activeclassname="active"
+          >
             Contact
           </NavLink>
-          <NavLink to="/booking" className="nav-item nav-link" activeclassname = "active">
-            Booking
-          </NavLink>
-          <NavLink to="/login" className="nav-item nav-link" activeclassname = "active">
-            Login
-          </NavLink>
-          <NavLink to="/register" className="nav-item nav-link" activeclassname = "active">
-            Register
-          </NavLink>
-          <NavLink to="/logout" className="nav-item nav-link">
-            Logout
-          </NavLink>
+          {isAuthenticated && (
+            <>
+              <NavLink
+                to="/booking"
+                className="nav-item nav-link"
+                activeclassname="active"
+              >
+                Booking
+              </NavLink>
+              <NavLink to="/logout" className="nav-item nav-link">
+                Logout
+              </NavLink>
+            </>
+          )}
+
+          {!isAuthenticated && (
+            <>
+              <NavLink
+                to="/login"
+                className="nav-item nav-link"
+                activeclassname="active"
+              >
+                Login
+              </NavLink>
+              <NavLink
+                to="/register"
+                className="nav-item nav-link"
+                activeclassname="active"
+              >
+                Register
+              </NavLink>
+            </>
+          )}
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
