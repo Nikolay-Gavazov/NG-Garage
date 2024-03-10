a8c3db82dbf592fa3b99ceca47a78df7e70024b4
Nikolay Gavazov <114027141+Nikolay-Gavazov@users.noreply.github.com>
2024-03-11 00:30:10 +0200

    Update spinner.jsx

--- src/components/shared/spinner.jsx
+++ src/components/shared/spinner.jsx
@@ -1,10 +1,10 @@
+import { PulseLoader } from "react-spinners";
+
 const Spinner = () => {
     return (
-        <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div className="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span className="sr-only">Loading...</span>
-            </div>
-        </div>
+        <div className="d-flex justify-content-center align-items-center">
+      <PulseLoader color="#3498db" loading={true} size={50} margin={"2px"} />
+    </div>
     )
 }
 
